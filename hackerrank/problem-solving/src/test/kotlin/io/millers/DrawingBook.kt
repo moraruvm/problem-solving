069a7e731f727bb146282dcd256e4722430f9482
Vlad Moraru <moraru.vm@gmail.com>
2022-06-07 16:10:08 +0300

    add DrawingBook problem

--- hackerrank/problem-solving/src/test/kotlin/io/millers/DrawingBook.kt
+++ hackerrank/problem-solving/src/test/kotlin/io/millers/DrawingBook.kt
@@ -1,6 +1,8 @@
 package io.millers
 
 import org.junit.jupiter.api.Test
+import kotlin.math.min
+import kotlin.test.assertEquals
 
 /**
  * https://www.hackerrank.com/challenges/drawing-book/problem
@@ -16,12 +18,28 @@ class DrawingBook {
      */
     fun pageCount(n: Int, p: Int): Int {
         // Write your code here
-        return 0
+
+        // pages are grouped as [1], [2,3], [4,5]
+        // last page is either [n-1, n] (odd) or [n] (even)
+
+        // the number of page 'groups'
+        val groupCount = n / 2
+        // the 'group' of the searched page is found through
+        val pageGroup = p / 2
+
+        return min(pageGroup, groupCount - pageGroup)
     }
 
     @Test
     fun test1() {
-        
+        val result = pageCount(6, 2)
+        assertEquals(1, result)
+    }
+
+    @Test
+    fun test2() {
+        val result = pageCount(5, 4)
+        assertEquals(0, result)
     }
 
 
